0c13a772ee9cb60449634a49ca720d6a2849c408
sandhiya <sandhiya4611@gmail.com>
2024-04-04 21:05:10 +0530

    maximum-nesting-depth-of-the-parentheses

---
+++ april4.java
@@ -0,0 +1,19 @@
+public class april4 {
+    class Solution {
+        public int maxDepth(String s) {
+             int depth = 0;
+            int maxDepth = 0;
+    
+            for (char c : s.toCharArray()) {
+                if (c == '(') {
+                    depth++;
+                    maxDepth = Math.max(maxDepth, depth);
+                } else if (c == ')') {
+                    depth--;
+                }
+            }
+    
+            return maxDepth;
+        }
+    } 
+}
